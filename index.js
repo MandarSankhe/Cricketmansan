eebb1091a8ffed22cd5d092baea001f895acaf3a
MandarSankhe <45758847+MandarSankhe@users.noreply.github.com>
2021-12-23 13:44:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,4 +7,26 @@ menu.addEventListener('click', function () {
     sidebar.classList.toggle('show-sidebar');
                     
 });
-  
+
+    if (localStorage.getItem('theme') == 'darkTheme') {
+            const htmlEl = document.getElementsByTagName('html')[0];
+            htmlEl.dataset.theme = "dark";
+            $("body").addClass("dark");
+            $(".changetheme").text("Light Mode");
+}
+
+
+        const htmlEl = document.getElementsByTagName('html')[0];
+        $(".changetheme").on("click", function () {
+            if ($("body").hasClass("dark")) {
+            $("body").removeClass("dark");
+                $(".changetheme").text("Dark Mode");
+                htmlEl.dataset.theme = "light";
+                localStorage.setItem('theme', 'lightTheme');
+            } else {
+            $("body").addClass("dark");
+                $(".changetheme").text("Light Mode");
+                htmlEl.dataset.theme = "dark";
+                localStorage.setItem('theme', 'darkTheme');
+            }
+        });
