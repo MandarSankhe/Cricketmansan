2a0f85c4e76e7ce31171113f1971966e9735a7cf
MandarSankhe <45758847+MandarSankhe@users.noreply.github.com>
2022-08-02 19:21:50 +0530

    Update search.js

--- search.js
+++ search.js
@@ -302,7 +302,7 @@ $("#searchInput").on("keydown", function () {
         source: data,
         select: function (event, ui) {
             $('#searchInput').val(ui.item.label);
-            window.open("/" + ui.item.link, "_self")
+            window.open("/Cricketmansan/" + ui.item.link, "_self")
             return false;
         },
 
@@ -315,7 +315,7 @@ $("#searchInput").on("keydown", function () {
 
 
         $img.attr({
-            src: "/" + item.icon,
+            src: "/Cricketmansan/" + item.icon,
             alt: item.label
         });
 
